a474c2a11713e308d93855a89afd24146609630d
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-06-03 18:54:07 +0300

    Add close button to popup

--- src/weather-button.js
+++ src/weather-button.js
@@ -2,11 +2,13 @@ import { renderWeatherPopup } from './weather-popup';
 
 export function renderWeatherButton (container, city) {
     container.innerHTML = `
-        <button class="wz-button">Show weather in ${city}!</button>
+        <button class="wz-button" ref-show-weather-btn>
+            Show weather in ${city}!
+        </button>
     `;
 
     container
-        .querySelector('button')
+        .querySelector('[ref-show-weather-btn]')
         .addEventListener('click', () => {
             renderWeatherPopup(container, city);
         });
--- src/weather-popup.js
+++ src/weather-popup.js
@@ -6,12 +6,27 @@ export function renderWeatherPopup(container, city) {
         popup.innerHTML = `
             <h1>Weather in ${city}</h1>
             <dl>
-                <dt>Temperature</dt><dd>${weatherData.main.temp}C</dd>
-                <dt>Wind speed</dt><dd>${weatherData.wind.speed} km/h</dd>
-                <dt>Humidity</dt><dd>${weatherData.main.humidity}%</dd>
-                <dt>Description</dt><dd>${weatherData.weather.map(w => w.main)}</dd>
+                <dt>Temperature</dt>
+                <dd>${weatherData.main.temp}C</dd>
+
+                <dt>Wind speed</dt>
+                <dd>${weatherData.wind.speed} km/h</dd>
+
+                <dt>Humidity</dt>
+                <dd>${weatherData.main.humidity}%</dd>
+
+                <dt>Description</dt>
+                <dd>${weatherData.weather.map(w => w.main)}</dd>
             </dl>
+            <button type="button" ref-close-btn>Close</button>
         `;
+
+        popup
+            .querySelector('[ref-close-btn]')
+            .addEventListener('click', () => {
+                container.removeChild(popup);
+            });
+
         container.appendChild(popup);
     });
 }
